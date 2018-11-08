df68f5009e1a86e0dd9f8fd18937175013184666
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-11-08 18:38:44 +0700

    Building Client view

--- Myjs.js
+++ Myjs.js
@@ -1,8 +1,13 @@
 getMusic();
 var music_content = document.getElementById("content");
 var play_song = document.getElementById("play");
+var getSong = "https://2-dot-backup-server-002.appspot.com/_api/v2/songs";
+var craeteSong = "https://2-dot-backup-server-002.appspot.com/_api/v2/songs";
+var login = "https://2-dot-backup-server-002.appspot.com/_api/v2/members/authentication";
+
+
 function getMusic() {
-    var APIurl = "https://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-free-songs";
+    var getSongFree = "https://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-free-songs";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -10,14 +15,14 @@ function getMusic() {
             var obj = JSON.parse(xmlHttp.responseText);
             for (var i = 0; i < obj.length; i++) {
                 var music_item = '<div class="container">';
-                music_item += '<div class="content" >';
+                music_item += '<div class="content"  onclick="PlayMusic(\'\' + obj[i].link + \'\')" \">';
                 music_item += '<div>';
-                music_item += '<h3 class="stt">'+ (i+1) +'</h3>';
+                music_item += '<h3 class="stt">' + (i + 1) + '</h3>';
                 music_item += '</div>';
-                music_item += '<div class="thumbnail" onclick="PlayMusic(\''+obj[i].link +'\')" style="background-image: url('+ obj[i].thumbnail +')"></div>';
+                music_item += '<div class="thumbnail" onclick="PlayMusic(\'' + obj[i].link + '\')" style="background-image: url(' + obj[i].thumbnail + ')"></div>';
                 music_item += '<div class="info-song">';
-                music_item += '<p class="name-song"> '+obj[i].name +'</p>';
-                music_item += '<p class="singer">'+ obj[i].singer+'</p>';
+                music_item += '<p class="name-song"> ' + obj[i].name + '</p>';
+                music_item += '<p class="singer">' + obj[i].singer + '</p>';
                 music_item += '</div>';
                 music_item += '<div class="btn_action">';
                 music_item += '<i class="fa fa-download" title="Tải về" aria-hidden="true"></i>';
@@ -28,11 +33,9 @@ function getMusic() {
                 music_item += '</div>';
                 music_content.innerHTML += music_item;
             }
-
         }
-
     };
-    xmlHttp.open("GET", APIurl, true);
+    xmlHttp.open("GET", getSongFree, true);
     xmlHttp.send();
 }
 
@@ -41,16 +44,28 @@ function PlayMusic(link) {
 }
 
 
+var btn = document.forms["Login-form"]["btn"];
+btn.onclick = function () {
+    var txt_email = document.forms["Login-form"]["email"];
+    var txt_pwd = document.forms["Login-form"]["password"];
 
-
-
-
-// '<li onclick="PlayMusic(\''+obj[i].link+'\')">';
-// music_item += '<p class="id"> ' + (i+1) + '</p>';
-// music_item += '<span>';
-// // music_item += '<img height="70px" src="'+ obj[i].thumbnail +'" alt="">';
-// music_item += '<div class="thumbnail" style="background: url('+obj[i].thumbnail +')"></div>';
-// music_item += '</span>';
-// music_item += '<label class="name">'+ obj[i].name + '</label>';
-// music_item += '</li>';
-// music_content.innerHTML += music_item;
+    var email = txt_email.value;
+    var password = txt_pwd.value;
+    var jsonObj = {
+        email: email,
+        password: password
+    };
+    console.log(jsonObj);
+    var data = JSON.stringify(jsonObj);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 201) {
+            var member = JSON.parse(xhttp.responseText);
+            alert(member.token);
+            localStorage.setItem(member.token);
+        }
+    };
+    xhttp.open("POST", login, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(data);
+};
